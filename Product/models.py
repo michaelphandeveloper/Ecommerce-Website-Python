5c6916b361832099038ba710d1fc4c6da4e127c6
henry6523 <111412170+henry6523@users.noreply.github.com>
2022-11-23 17:12:14 +0700

    Update Product

--- Product/models.py
+++ Product/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from .models import Category
 
 # Product
 class Product(models.Model):
     image = models.ImageField()
     details = models.TextField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    brand = models.ForeignKey(Category,on_delete=models.CASCADE)
+    color = models.ForeignKey(Category,on_delete=models.CASCADE)
+    size = models.ForeignKey(Category,on_delete=models.CASCADE)
+    materials = models.ForeignKey(Category,on_delete=models.CASCADE)
 
     class Meta:
         db_table = "Product"
@@ -11,53 +17,4 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-# Brand
-class Brand(models.Model):
-    title = models.CharField(max_length=50)
-    image = models.ImageField(upload_to="brand_imgs/")
 
-    class Meta:
-        db_table = "Brand"
-
-    def __str__(self):
-        return self.title
-
-# Color
-class Color(models.Model):
-    title = models.CharField(max_length=50)
-    color_code = models.CharField(max_length=20)
-
-    class Meta:
-        db_table = "Color"
-
-    def __str__(self):
-        return self.title
-
-# Size
-class Size(models.Model):
-    title = models.CharField(max_length=50)
-
-    class Meta:
-        db_table = "Size"
-
-    def __str__(self):
-        return self.title
-
-# Price
-class Price(models.Model):
-    title = models.FloatField(max_length=20)
-
-    class Meta:
-        db_table = "Price"
-
-    def __str__(self):
-        return self.title
-
-#Materials
-class Materials(models.Model):
-    description = models.Model(max_length=50)
-
-    class Meta:
-        db_table = "Materials"
-    def __str__(self):
-        return self.title  
