58f841ab09224ae7d836da5bf3ec97315e4f59d7
michaelphandeveloper <ankhephanhoa1703@gmail.com>
2022-11-23 22:23:16 +0700

    update

--- .history/core/settings_20221123222132.py
+++ .history/core/settings_20221123222132.py
@@ -26,9 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Category',
-    'Product',
-    'User'
+    "Category",
+    "Product"
 ]
 
 MIDDLEWARE = [
@@ -46,7 +45,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
