8471e711ccb7059e2210b49fe4548957a9bacbf3
michaelphandeveloper <ankhephanhoa1703@gmail.com>
2022-11-23 14:38:25 +0700

    new changes

--- .history/category/models_20221123143105.py
+++ .history/category/models_20221123143105.py
@@ -1,10 +1,11 @@
 from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=25)
-    description = models.TextField()
+    description = models.TextField(max_length=250)
 
     class Meta:
         db_table = "category"
 
     def __str__(self):
         return self.name
+
