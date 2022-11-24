23e442f22603f0d4b4a69606827f4e244a476e49
michaelphandeveloper <ankhephanhoa1703@gmail.com>
2022-11-24 22:50:21 +0700

    update frondend using react framework

---
+++ .history/User/models_20221124220027.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class lastname(models.Model):
+  lastname = models.TextField(max_length=16)
+
+class firstname(models.Model):
+  firstname = models.TextField(max_length=16)
+
+class phone(models.Model):
+  phone = models.FloatField(max_length=11)
+
+class email(models.Model):
+  email = models.EmailField(max_length=25)
+
+class gender(models.Model):
+  gender = models.TextField(max_length=8)
+
+class address(models.Model):
+  address = models.CharField(max_length=25)  
+
