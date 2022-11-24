89c2f5ce2f0d229db5e864380e54f7bc82705a7a
henry6523 <111412170+henry6523@users.noreply.github.com>
2022-11-24 21:48:15 +0700

    Update product and fix some errors

--- Product/admin.py
+++ Product/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'brand', 'color', 'size', 'material')
+
+admin.site.register(Product, ProductAdmin)
