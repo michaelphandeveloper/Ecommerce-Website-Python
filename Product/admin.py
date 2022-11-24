ffe4269172381a1fa4cbb3fbcf9694c475664ac1
henry6523 <111412170+henry6523@users.noreply.github.com>
2022-11-24 22:49:33 +0700

    Update product attribute and banner

--- Product/admin.py
+++ Product/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Product
+from .models import ProductAttribute
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'brand', 'color', 'size', 'material')
 
 admin.site.register(Product, ProductAdmin)
+
+#Product Attribute
+class ProductAttributeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'color', 'size', 'price')
+admin.site.register(ProductAttribute, ProductAttributeAdmin)
--- Product/models.py
+++ Product/models.py
@@ -18,3 +18,12 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+# Product Attribute
+class ProductAttribute(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    color = models.ForeignKey(Color,on_delete=models.CASCADE)
+    size = models.ForeignKey(Size,on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+         return self.product.title
