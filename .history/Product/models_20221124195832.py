495bc73bcacee7efca33a95c383833a91001f6d3
michaelphandeveloper <ankhephanhoa1703@gmail.com>
2022-11-24 20:07:46 +0700

    Fix minor bugs

---
+++ .history/Product/models_20221124195832.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Product
+class Product(models.Model):
+    image = models.ImageField(upload_to="product_imgs/")
+    details = models.TextField()
+
+    class Meta:
+        db_table = "Product"
+
+    def __str__(self):
+        return self.title
+
+
