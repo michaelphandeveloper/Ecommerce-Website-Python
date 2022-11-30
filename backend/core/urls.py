6ab50dde18a3a1b978c373a87ae527eabdd26a74
michaelphandeveloper <ankhephanhoa1703@gmail.com>
2022-11-30 14:14:48 +0700

    Update

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
+    path('index', views.index),
 ]
