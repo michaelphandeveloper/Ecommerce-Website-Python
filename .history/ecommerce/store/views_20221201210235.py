c166b4b7be6e5b79aa5d8b99f4925514b38fa46a
michaelphandeveloper <ankhephanhoa1703@gmail.com>
2022-12-01 21:25:07 +0700

    update 4: add user cart

--- .history/ecommerce/store/views_20221201210235.py
+++ .history/ecommerce/store/views_20221201210235.py
@@ -9,15 +9,11 @@ def store(request):
 
 def cart(request):
 
-	if request.user.is_authenticated:
+	if request.user.is.authenticated:
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
-	else:
-		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
 
-	context = {'items':items, 'order':order}
+	context = {}
 	return render(request, 'store/cart.html', context)
 
 def checkout(request):
