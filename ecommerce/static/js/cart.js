afd17feee49727c94197b2c58e8a134b7a970359
henry6523 <111412170+henry6523@users.noreply.github.com>
2022-12-06 09:07:23 +0700

    Update add and remove items

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -8,12 +8,39 @@ for(i = 0; i < updateBtns.length; i++) {
 
         console.log('user: ', user)
         if(user === 'AnonymousUser') {
-            console.log('You need to login!')
+            addCookieItem(productId, action)
         }else {
             updateUserOrder(productId, action)
         }
     })
 }
+
+function addCookieItem(productId, action){
+    console.log('Not logged in...')
+
+    if (action == 'add'){
+        if (cart[productId] == undefined){
+            cart[productId] = {'quantity':1}
+
+        }else {
+            cart[productId]['quantity'] += 1
+        }
+    }
+
+    if (action == 'remove'){
+        cart[productId]['quantity'] -= 1
+
+        if(cart[productId]['quantity'] <= 0){
+            console.log('Item has been removed from your cart')
+            delete car[productId]
+        }
+    }
+    console.log('Cart:', cart)
+    document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
+}
+
+
 
 function updateUserOrder(productId, action) {
     console.log('Logged in')
