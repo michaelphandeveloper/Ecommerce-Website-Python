cbeea62805026cbf0573f8a50ca38c655fefcff4
michaelphandeveloper <ankhephanhoa1703@gmail.com>
2022-12-21 19:55:57 +0700

    Update Logout Button & Products

---
+++ .history/ecommerce/store/views_20221221192552.py
@@ -0,0 +1,140 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.http import HttpResponse
+import json
+import datetime
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from .models import *
+from .forms import CreateUserForm
+from .utils import cookieCart, cartData, guestOrder
+
+# Create your views here.
+def registerPage(request):
+	form = CreateUserForm()
+	if request.method == 'POST':
+		form = CreateUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			user = User.objects.get(username = username)
+			customer = Customer (email=form.cleaned_data.get('email'), user = user)
+			customer.save()
+			messages.success(request, 'Account was created for ' + username)
+
+			return redirect('login')
+
+	context = {'form':form}
+	return render(request, 'store/register.html', context)
+
+def loginPage(request):
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password = request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+		
+			if user is not None:
+				login(request, user)
+				return redirect('store')
+
+			else:
+				messages.info(request, 'Username or Password is incorrect')
+
+		context = {}
+		return render(request, 'store/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('store')
+
+def store(request):
+	data = cartData(request)
+	cartItems = data['cartItems']
+
+	products = Product.objects.all()
+	context = {'products':products, 'cartItems': cartItems}
+	return render(request, 'store/store.html', context)
+
+@login_required(login_url='login')
+def cart(request):
+	data = cartData(request)
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	context = {'items':items, 'order':order, 'cartItems': cartItems}
+	return render(request, 'store/cart.html', context)
+
+@login_required(login_url='login')
+def checkout(request):
+	data = cartData(request)
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	context = {'items':items, 'order':order, 'cartItems': cartItems}
+	return render(request, 'store/checkout.html', context)
+
+@login_required(login_url='login')
+def updateItem(request):
+		data = json.loads(request.body)
+		productId = data['productId']
+		action = data['action']
+		
+		print('Action:', action)
+		print('productId:', productId)
+
+		customer = request.user.customer
+		product = Product.objects.get(id=productId)
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+		orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+		if action == 'add':
+			orderItem.quantity = (orderItem.quantity + 1)
+		elif action == 'remove':
+			orderItem.quantity = (orderItem.quantity - 1)
+
+		orderItem.save()
+
+		if orderItem.quantity <= 0:
+			orderItem.delete()
+
+		return JsonResponse('Item was added!!', safe=False)
+
+@login_required(login_url='login')
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+
+	else:
+		customer, order = guestOrder(request, data)
+
+	total = float(data['form']['total'])
+	order.transaction_id = transaction_id
+
+	if total == float(order.get_cart_total):
+		order.complete = True
+		order.save()
+
+	if order.shipping == True:
+			ShippingAddress.objects.create(
+				customer=customer,
+				order=order,
+				address=data['shipping']['address'],
+				city=data['shipping']['city'],
+				state=data['shipping']['state'],
+				zipcode=data['shipping']['zipcode'],
+			)
+
+	return JsonResponse('Payment complete!', safe=False)
